b0cd7c4e63c119879a5721eb1943d9bc51b375a1
D'Amour <damourba@gmail.com>
2017-07-20 19:24:57 -0400

    added route for streamkit

--- routes/streamkit.js
+++ routes/streamkit.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nintendude92' });
+    res.render('streamkit', {title: 'Streamkit'});
 });
 
 module.exports = router;
