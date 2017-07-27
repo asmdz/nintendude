ac092e012195263dbc397e8284c9551d14509c18
D'Amour <damourba@gmail.com>
2017-07-26 21:33:37 -0400

    continued development of oauth code flow

--- routes/streamkit.js
+++ routes/streamkit.js
@@ -1,31 +1,62 @@
 var express = require('express');
 var router = express.Router();
+
+var request = require('request');
+var crypto = require('crypto');
+
 // TODO: Change how config is read in.
 var config = require('../config/config.json');
 var auth = require('../config/auth.json');
 
+
 router.get('/', function(req, res, next) {
-    // Check if request is from redirect uri of user.
-    console.log(req.query);
-    if (req.query.hasOwnProperty('code')) {
-
-        res.render('streamkit', {title: 'Success', 'oauth_url': oauthUrl});
-        return;
-    }
-
     // TODO: Set up seperate code for dev and prd environments.
     var oauthParams = {
         'client_id': auth.client_id,
-        'redirect_uri': "http://localhost:3000" + config.redirect_uri,
+        'redirect_uri': 'http://localhost:3000' + '/streamkit/dashboard',
         'response_type': "code",
         'scope': config.scope,
-        'force_verify': true
+        'force_verify': true,
+        'state': generateToken()
     };
     var oauthUrl = 'https://api.twitch.tv/kraken/oauth2/authorize'
             + getQueryString(oauthParams);
 
     res.render('streamkit', {title: 'Streamkit', 'oauth_url': oauthUrl});
 });
+
+
+router.get("/dashboard", function(req, res, next) {
+    console.log(req);
+    console.log(req.query);
+
+    // Make request for user token.
+    var params = {
+        'client_id': auth.client_id,
+        'client_secret': auth.secret,
+        'code': req.query.code,
+        'grant_type': 'authorization_code',
+        'redirect_uri': 'http://localhost:3000' + '/streamkit/dashboard',
+        'state': req.query.state
+    };
+
+    var reqUrl = 'https://api.twitch.tv/kraken/oauth2/token'
+            + getQueryString(params);
+
+    console.log(reqUrl);
+
+    // FIXME parameters should be in post body, otherwise we get 404.
+    request(reqUrl, function(err, res, body) {
+        if (!err && res.statusCode == 200) {
+            console.log(body);
+        }
+        else {
+            console.log(err);
+            next(err);
+        }
+    });
+});
+
 
 /**
  * Converts an object to a query string.
@@ -47,6 +78,15 @@ function getQueryString(obj) {
     }
 
     return queryStr;
+}
+
+
+/**
+ * Generates a random token.
+ * @returns {string} Random token.
+ */
+function generateToken() {
+    return crypto.randomBytes(10).toString('hex');
 }
 
 module.exports = router;
